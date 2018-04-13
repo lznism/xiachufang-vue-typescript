09b2b0120ebc2cbaffaba56a81c9791bf4027895
lznism <lzguo@whu.edu.cn>
2018-04-13 11:44:31 +0800

    删除一些不必要的文件

---
+++ src/components/header.vue
@@ -0,0 +1,42 @@
+<template>
+    <header class="xcf-header">
+        <i class="iconfont icon-zuojiantou" @click="back()"></i>
+        <img src="../assets/images/logo.png">
+    </header>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({})
+export default class Header extends Vue {
+    back() {
+        this.$router.go(-1);
+    }
+}
+</script>
+<style lang="scss" scoped>
+@function r($px) {
+    @return $px / 75 + rem;
+}
+.xcf-header {
+    height: r(121);
+    line-height: r(121);
+    border-bottom: 1px solid #f6f6f6;
+    position: relative;
+    font-size: 0;
+
+    .iconfont {
+        font-size: r(28);
+        color: #383835;
+        margin-left: r(32);
+    }
+
+    img {
+        height: r(55);
+        position: absolute;
+        top: r(32);
+        left: r(74);
+    }
+}
+</style>
